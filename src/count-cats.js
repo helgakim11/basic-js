72d1b9334fc737b31e97e3e79ee38bb5c0ac610d
Helga Maznikava <helga@helgas-air.lan>
2020-10-30 15:58:58 -0700

    feat: add count-cats and dream-team solutions

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let catsCount = 0;
+  if (matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+      var subArray = matrix[i];
+      for(let j = 0; j < subArray.length; j++) {
+        if (subArray[j] === '^^') {
+          catsCount += 1;
+        }
+      }
+    }
+  }
+  return catsCount;
 };
+
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) { return false };
+  let names = members.filter(member => typeof(member) === "string" &&
+                                    isNaN(parseInt(member)));
+  names = names.map(item => item.trim().toLowerCase());  
+  names.sort();
+  let result = [];
+  for (let i = 0; i < names.length; i++) {
+      result.push((names[i])[0].toUpperCase());
+  }
+  return result.join('');
 };
